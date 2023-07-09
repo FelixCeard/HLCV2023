964aa019305a7daf9151cb1145a8cf3e40664e3a
Felix Ceard-Falkenberg <fece00001@stud.uni-saarland.de>
2023-07-09 17:38:38 +0200

    test docker

--- execute.sh
+++ execute.sh
@@ -1,4 +1,4 @@
 # Path to the `.py` file you want to run
-PYTHON_SCRIPT_PATH="/home/hlcv_team12/HLCV2023/"
+PYTHON_SCRIPT_PATH="/home/hlcv_team017/HLCV2023/"
 # Path to the Python binary of the conda environment
-CONDA_PYTHON_BINARY_PATH="/home/hlcv_team12/miniconda3/envs/hlcv-ss23/bin/python"
+CONDA_PYTHON_BINARY_PATH="/home/hlcv_team017/miniconda3/envs/hlcv-ss23/bin/python"
