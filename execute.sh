db7c8a4433b2a262810e16f20c4df02646ba4aa2
Felix Ceard-Falkenberg <fece00001@stud.uni-saarland.de>
2023-07-09 19:34:11 +0200

    ups

--- execute.sh
+++ execute.sh
@@ -2,3 +2,6 @@
 PYTHON_SCRIPT_PATH="/home/hlcv_team017/HLCV2023/"
 # Path to the Python binary of the conda environment
 CONDA_PYTHON_BINARY_PATH="/home/hlcv_team017/miniconda3/envs/hlcv-ss23/bin/python"
+
+cd $PYTHON_SCRIPT_PATH
+$CONDA_PYTHON_BINARY_PATH "$@"
